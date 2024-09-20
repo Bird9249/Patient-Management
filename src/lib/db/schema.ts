e89138a5ffad1effc30b5f45821e7739383b11ae
nadaongam <lattanadaongam@gmial.com>
2024-09-20 15:04:55 +0700

    add relationship every table on schema

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,6 +1,8 @@
+import { relations } from 'drizzle-orm';
 import { date, integer, pgEnum, pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
-export const statusEnum = pgEnum('status', ['scheduled', 'pending', 'cancelled']);
 
+export const statusEnum = pgEnum('status', ['scheduled', 'pending', 'cancelled']);
+export const genderEnum = pgEnum ('gender', ['male', 'female', 'other'])
 // Table Definitions
 export const account = pgTable('account', {
   id: serial('id').primaryKey(),
@@ -9,29 +11,59 @@ export const account = pgTable('account', {
   phone: varchar('phone', { length: 20 }).unique().notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
 });
+export const accountRelations = relations(account, ({ one, many }) => ({
+  userInfo: one(userInfo, {
+    fields: [account.id],
+    references: [userInfo.accountId]
+  }),
+  appointments: many(appointment)
+}));
+
 export const userInfo = pgTable('userinfo', {
   id: serial('id').primaryKey(), 
   accountId: integer('account_id').references(() => account.id, {onDelete:'cascade'}).notNull(),
   dateOfBirth: date('date_of_birth', { mode: "string" }).notNull(),
-  gender: text('gender').notNull(),
+  gender: genderEnum('gender').notNull(),
   address: text('address').notNull(),
-  occupation: text('occupation').notNull(),
+  occupation: varchar('occupation', {length:255}).notNull(),
   emergencyName: varchar('emergency_name', { length: 255 }).notNull(),
   emergencyPhone: varchar('emergency_phone', { length: 20 }).notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
 });
+export const userInfoRelations = relations(userInfo, ({ one, many }) => ({
+  account: one(account, {
+    fields: [userInfo.accountId],
+    references: [account.id]
+  }),
+  identify: one(identify, {
+    fields: [userInfo.id],
+    references: [identify.userinfoId]
+  }),
+  medicalInfo: one(medicalInfo, {
+    fields: [userInfo.id],
+    references: [medicalInfo.userinfoId]
+  }),
+}));
+
 export const identify = pgTable('identify', {
   id: serial ('id').primaryKey(),
-  userId: integer ('user_id').references(()=> userInfo.id, {onDelete:'cascade'}).notNull(),
-  name: text('name').notNull(),
+  userinfoId: integer ('userinfo_id').references(()=> userInfo.id, {onDelete:'cascade'}).notNull(),
+  name: varchar('name', {length:255}).notNull(),
   number: varchar('number', { length: 255 }).notNull(),
   image: text('image').notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull()
 });
+export const identifyRelations = relations(identify, ({ one }) => ({
+  userInfo: one(userInfo, {
+    fields: [identify.userinfoId],
+    references: [userInfo.id]
+  }),
+}));
+
 export const medicalInfo = pgTable('medicalinfo', {
   id: serial('id').primaryKey(),
-  userId: integer('user_id').references(()=> userInfo.id, {onDelete:'cascade'}).notNull(),
-  doctorId: integer('doctor_id').references(() => doctor.id, {onDelete:'cascade'}),
+  userinfoId: integer('userinfo_id').references(()=> userInfo.id, {onDelete:'cascade'}).notNull(),
+  doctorId: integer('doctor_id').references(() => doctor.id, {onDelete: 'set null'}),
   insuranceName: varchar('insurance_name', { length: 255 }),
   insurancePhone: varchar('insurance_phone', { length: 20 }),
   allergies: text('allergies'),
@@ -40,6 +72,16 @@ export const medicalInfo = pgTable('medicalinfo', {
   medicalHistory: text('medical_history').notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
 });
+export const medicalInfoRelations = relations(medicalInfo, ({ one }) => ({
+  userInfo: one(userInfo, {
+    fields: [medicalInfo.userinfoId],
+    references: [userInfo.id]
+  }),
+  doctor: one(doctor, {
+    fields: [medicalInfo.doctorId],
+    references: [doctor.id]
+  }),
+}));
 
 export const doctor = pgTable('doctor', {
   id: serial('id').primaryKey(),
@@ -48,13 +90,27 @@ export const doctor = pgTable('doctor', {
   image: text('image').notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
 });
+export const doctorRelations = relations(doctor, ({ many }) => ({
+  appointments: many(appointment),
+  medicalInfos: many(medicalInfo),
+}));
 
 export const appointment = pgTable('appointment', {
   id: serial('id').primaryKey(),
-  accountId: integer('account_id').references(() => account.id, {onDelete:'cascade'}).notNull(),
+  accountId: integer('account_id').references(() => account.id, {onDelete:'set null'}).notNull(),
   reasonOfAppointment: text('reason_of_appointment').notNull(),
   dateTime: timestamp('date_time').notNull(),
-  doctorId: integer('doctor_id').references(() => doctor.id, {onDelete:'cascade'}).notNull(),
+  doctorId: integer('doctor_id').references(() => doctor.id, {onDelete:'set null'}).notNull(),
   status: statusEnum('status').notNull().default('pending'),
   createdAt: timestamp('created_at').defaultNow().notNull(),
 });
+export const appointmentRelations = relations(appointment, ({ one }) => ({
+  account: one(account, {
+    fields: [appointment.accountId],
+    references: [account.id]
+  }),
+  doctor: one(doctor, {
+    fields: [appointment.doctorId],
+    references: [doctor.id]
+  }),
+}));
