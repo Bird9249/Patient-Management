ec9304354637577419eed159d8dadeb60f07458f
nadaongam <lattanadaongam@gmial.com>
2024-09-23 17:10:00 +0700

    make transaction in userinfo, identify, medicalinfo

--- src/routes/information/Actions/userInfo.tsx
+++ src/routes/information/Actions/userInfo.tsx
@@ -1,22 +1,42 @@
 import { db } from "../../../lib/db/db";
-import { userInfo } from "../../../lib/db/schema";
+import { identify, medicalInfo, userInfo } from "../../../lib/db/schema";
 import { IRegisterSchema } from "../schema/register";
 
 export async function addUserInfo(data: IRegisterSchema) {
-  const [{ id }] = await db
-    .insert(userInfo)
-    .values({
-      accountId: data.userInfo.accountId,
-      dateOfBirth: data.userInfo.dayOfBirth,
-      gender: data.userInfo.gender,
-      address: data.userInfo.address,
-      occupation: data.userInfo.occupation,
-      emergencyName: data.userInfo.emergencyName,
-      emergencyPhone: data.userInfo.emergencyPhone,
-    })
-    .returning({
-      id: userInfo.id,
+  return await db.transaction(async (tx) => {
+    const [{ id }] = await tx
+      .insert(userInfo)
+      .values({
+        accountId: data.userInfo.accountId,
+        dateOfBirth: data.userInfo.dayOfBirth,
+        gender: data.userInfo.gender,
+        address: data.userInfo.address,
+        occupation: data.userInfo.occupation,
+        emergencyName: data.userInfo.emergencyName,
+        emergencyPhone: data.userInfo.emergencyPhone,
+      })
+      .returning({
+        id: userInfo.id,
+      });
+
+    await tx.insert(medicalInfo).values({
+      userinfoId: id,
+      doctorId: data.medicalInfo.doctorId,
+      insuranceName: data.medicalInfo.insuranceName,
+      insurancePhone: data.medicalInfo.insurancePhone,
+      allergies: data.medicalInfo.allergies,
+      currentMedication: data.medicalInfo.currentMedication,
+      familyMedicalHistory: data.medicalInfo.familyMedicalHistory,
+      medicalHistory: data.medicalInfo.medicalHistory,
     });
 
-  return id;
+    await tx.insert(identify).values({
+      userinfoId: id,
+      type: data.identify.type,
+      number: data.identify.number,
+      image: data.identify.image,
+    });
+
+    return id;
+  });
 }
