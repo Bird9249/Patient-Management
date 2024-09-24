c6cdb6ee0ea9115eaafdc0ba5b36d6360c3b8619
nadaongam <lattanadaongam@gmial.com>
2024-09-24 15:33:44 +0700

    add  accountSchema in IregisterSchema

--- src/routes/information/schema/register.tsx
+++ src/routes/information/schema/register.tsx
@@ -1,9 +1,11 @@
 import { InferInput, object } from "valibot";
+import { AccountSchema } from "./account";
 import { IdentifySchema } from "./identify";
 import { MedicalInfoSchema } from "./medicalInfo";
 import { UserInfoSchema } from "./userInfo";
 
 export const RegisterSchema = object({
+  account: AccountSchema,
   userInfo: UserInfoSchema,
   identify: IdentifySchema,
   medicalInfo: MedicalInfoSchema,
