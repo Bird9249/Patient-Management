d070e55e3a58fc2d1615960bc61a3ea5a432ae71
ajia42 <ajiathavisay3151@gmail.com>
2024-09-25 10:14:49 +0700

    create valibot, action file and create useRegisterAction = formAction$ in index file

--- src/routes/login/action/actions.tsx
+++ src/routes/login/action/actions.tsx
@@ -1,6 +1,12 @@
 import { db } from "~/lib/db/db";
 import { IAccountSchema } from "../schema/account";
+import { account } from "~/lib/db/schema";
 
 export async function addAccount(data: IAccountSchema) {
-  //   await db.insert(account);
+  const result = await db
+    .insert(account)
+    .values({ name: data.name, email: data.email, phone: data.phone })
+    .returning({ id: account.id });
+
+  return result[0].id;
 }
--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -3,6 +3,36 @@ import logo_image from "./image/logo_SnatBas.jpg";
 import { TextInput } from "~/components/forms/text-input/TextInput";
 import { Button } from "~/components/button/Button";
 import login_image from "./image/login_page.jpg";
+import { LuUserCircle } from "@qwikest/icons/lucide";
+import { formAction$, valiForm$ } from "@modular-forms/qwik";
+import { AccountSchema, IAccountSchema } from "./schema/account";
+import { addAccount } from "./action/actions";
+
+export const useRegisterAction = formAction$<
+  IAccountSchema,
+  { success: boolean; message: string; id?: number }
+>(async (values) => {
+  try {
+    const result = await addAccount(values);
+
+    return {
+      data: {
+        success: true,
+        message: "",
+        id: result,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      data: {
+        success: false,
+        message: (error as Error).message,
+      },
+    };
+  }
+}, valiForm$(AccountSchema));
 
 export default component$(() => {
   return (
@@ -26,29 +56,13 @@ export default component$(() => {
 
               <div class="space-y-8">
                 <TextInput
-                  icon={
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="15"
-                      height="15"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="lucide lucide-circle-user-round"
-                    >
-                      <path d="M18 20a6 6 0 0 0-12 0" />
-                      <circle cx="12" cy="10" r="4" />
-                      <circle cx="12" cy="12" r="10" />
-                    </svg>
-                  }
+                  icon={<LuUserCircle />}
                   value=""
                   label="Full name"
                   type="text"
                   name="fullName"
                   size="small"
+                  placeholder="  Valmon Borglum"
                 />
 
                 <TextInput
@@ -74,6 +88,7 @@ export default component$(() => {
                   type="text"
                   name="email"
                   size="small"
+                  placeholder="  some123@gmail.com"
                 />
 
                 <TextInput
@@ -93,11 +108,19 @@ export default component$(() => {
                       <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
                     </svg>
                   }
-                  label="Phone number "
+                  label="Your phone number"
                   value=""
                   type="text"
                   name="phoneNumber"
                   size="small"
+                  placeholder="  +856 20 xxx xxx xx"
+                />
+                <TextInput
+                  type="password"
+                  label="Enter your password"
+                  name="password"
+                  size="small"
+                  placeholder="Enter your password"
                 />
                 <Button block variant="solid" type="submit">
                   Get started
