de7da00e73cb10cd8da3ed00b89187be5e9e9679
sanxay <thachmanivong@gmail.com>
2024-09-26 15:20:11 +0700

    Add field in appointment

--- src/routes/appointment/index.tsx
+++ src/routes/appointment/index.tsx
@@ -1,14 +1,49 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, qrlDEV } from '@builder.io/qwik';
 import { Button } from '~/components/button/Button';
 import { AdvancedSelect, SelectOption } from '~/components/forms/advanced-select/AdvancedSelect';
 import { TextInput } from '~/components/forms/text-input/TextInput';
 import { Textarea } from '~/components/forms/textarea/Textarea';
 import logo_page from '/public/logo project.png';
 import backgroundpage from '/public/background (1).jpg';
+import { Form, routeLoader$ } from '@builder.io/qwik-city';
+import { db } from '~/lib/db/db';
+import { ImageProps } from '@unpic/qwik';
+import {AppointmentSchema, IAppointmentSchema } from './schema/appointment'
+import { appointment } from '~/lib/db/schema';
+import { setValue, useForm, valiForm$ } from '@modular-forms/qwik';
+
+export const useDoctorLoader = routeLoader$(async () => {
+    return await db.query.doctor.findMany({
+        columns: {
+         id: true,
+         name: true,
+         image: true,   
+        }
+    })
+
+})
 
 export default component$(() => {
+const loader = useDoctorLoader();
+
+const [form, {Field, Form}] = useForm<IAppointmentSchema>({
+    loader : {value: {
+            reasonOfAppointment: "",
+            dateTime: "",
+            doctorId: 0,
+            status: "PENDING",
+            comment: "", 
+    }},
+    validate: valiForm$(AppointmentSchema)
+});
+console.log(loader.value);
+
     return (
-        <div class="bg-white min-h-screen flex flex-col justify-center relative">
+        <Form
+        onSubmit$={async (values) =>{
+// await
+        }}>
+<div class="bg-white min-h-screen flex flex-col justify-center relative">
             <img class="absolute w-screen h-screen brightness-110" src={backgroundpage} alt="" />
            <div class="z-10">
                     <div class="ml-28 mb-12">
@@ -21,44 +56,82 @@ export default component$(() => {
                     <div class="bg-gray-50 rounded-lg p-8 w-full max-w-screen-lg text-black">
                         <div class="mb-4">
                             {/* Dropdown */}
-                            <AdvancedSelect
-                                name="doctor"
-                                placeholder="Select your doctor"
-                                required
-                                label="Doctor"
-                            />
+                            <Field name="doctorId" type='number'>
+                                {(field, props) =>(
+                                    <AdvancedSelect
+                                        name={props.name}
+                                        options={loader.value.map(({id, name, image})=> ({
+                                            label: name,
+                                            value: id,
+                                            img: import.meta.env.PUBLIC_IMAGE_URL + "/" + image,
+                                        }),)}
+                                        onSelected$={(val) => {
+                                            setValue(form, "doctorId", val as number);
+                                        }}
+                                        value={field.value}
+                                        error={field.error}
+                                        placeholder="Select your doctor"
+                                        required
+                                        label="Doctor"
+                                    />)}
+                            
+                            </Field>
+                            
                             <br />
                             {/* textarea */}
                             <div class="flex flex-row space-x-6">
                                 <div class="flex-1">
-                                    <Textarea
-                                        name="reason"
-                                        label="Reason for appointment"
-                                        placeholder="ex: Annual monthly check-up"
-                                        size='large'
-                                    />
+                                    <Field name= 'reasonOfAppointment'>
+                                        {(field, props) => (
+                                            <Textarea
+                                                {...props}
+                                                value={field.value}
+                                                error={field.error}
+                            
+                                                label="Reason for appointment"
+                                                placeholder="ex: Annual monthly check-up"
+                                                size='large'
+                                            />
+                                        )}
+                                    </Field> 
                                 </div>
 
                                 <div class="flex-1">
-                                    <Textarea
-                                        name="comments"
-                                        label="Additional comments/notes"
-                                        placeholder="ex: Prefer afternoon appointments, if possible"
-                                        size='large'
-                                    />
+                                    <Field name="comment">
+                                        {(field, props) => (
+                                            <Textarea
+                                                {...props}
+                                                value={field.value}
+                                                error={field.error}
+
+                                                label="Additional comments/notes"
+                                                placeholder="ex: Prefer afternoon appointments, if possible"
+                                                size='large'
+                                            />
+                                        )}
+                                    </Field>
                                 </div>
                             </div>
                             <br />
-                            <TextInput
-                                label="Expected appointment date"
-                                type="datetime-local"
-                            />
+
+                            <Field name="dateTime">
+                                {(field, props) => (
+                                    <TextInput
+                                        {...props}
+                                        value={field.value}
+                                        error={field.error}
+                                        
+                                        label="Expected appointment date"
+                                        type="datetime-local"
+                                    />
+                                )}
+                            </Field>
                             <br />
 
                             <Button 
                                 block
                                 variant="solid" 
-                                type="button"
+                                type="submit"
                                 >
                                 Submit and continue
                             </Button>
@@ -67,5 +140,6 @@ export default component$(() => {
                 </div>    
            </div>
         </div>
+        </Form>
     )
 });
