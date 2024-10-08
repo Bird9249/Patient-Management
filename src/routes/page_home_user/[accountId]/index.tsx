1e91160134c75da953f5f6d3357df1802d6c4a51
ajia42 <ajiathavisay3151@gmail.com>
2024-10-08 10:34:40 +0700

    be able to log out into log_in page

--- src/routes/page_home_user/[accountId]/index.tsx
+++ src/routes/page_home_user/[accountId]/index.tsx
@@ -3,6 +3,7 @@ import logo_image from "/logo project.png";
 import {
   Link,
   RequestHandler,
+  routeAction$,
   routeLoader$,
   useLocation,
   useNavigate,
@@ -99,6 +100,14 @@ export const useAppointmentHistoryLoader = routeLoader$(
   },
 );
 
+export const useLogoutAction = routeAction$(async (values, { cookie }) => {
+  cookie.delete("auth-token", { path: "/" });
+
+  return {
+    success: true,
+  };
+});
+
 export default component$(() => {
   const loader = useAppointmentHistoryLoader();
   const nav = useNavigate();
@@ -108,6 +117,7 @@ export default component$(() => {
     url: { searchParams },
   } = useLocation();
   const isOpen = useSignal<boolean>(false);
+  const logoutAction = useLogoutAction();
 
   const doctorCol = $(({ doctor }: AppointmentResponse) => (
     <div class="flex gap-x-2 px-4 py-2">
@@ -124,18 +134,58 @@ export default component$(() => {
 
   const statusCol = $(({ status }: AppointmentResponse) =>
     status === "pending" ? (
-      <span class="inline-flex items-center gap-x-1.5 rounded-full bg-blue-600 px-3 py-1.5 text-xs font-medium text-white dark:bg-blue-500">
-        <LuHourglass />
+      <span class=" inline-flex items-center gap-x-1.5 rounded-full bg-cyan-300 px-3 py-1.5 text-xs font-medium text-cyan-700">
+        <svg
+          width="15"
+          height="18"
+          viewBox="0 0 15 18"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M2.5 13H9.5M2.5 5H9.5M8.5 13V11.3312C8.49994 11.119 8.39455 10.9156 8.207 10.7656L6 9M6 9L3.793 10.7656C3.60545 10.9156 3.50006 11.119 3.5 11.3312V13M6 9L3.793 7.2344C3.60545 7.08441 3.50006 6.88096 3.5 6.6688V5M6 9L8.207 7.2344C8.39455 7.08441 8.49994 6.88096 8.5 6.6688V5"
+            stroke="#0E7790"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
         pending
       </span>
     ) : status === "scheduled" ? (
-      <span class="inline-flex items-center gap-x-1.5 rounded-full bg-teal-500 px-3 py-1.5 text-xs font-medium text-white">
-        <LuCheck />
+      <span class=" inline-flex items-center gap-x-1.5 rounded-full bg-teal-400 px-3 py-1.5 text-xs font-medium text-teal-800">
+        <svg
+          width="14"
+          height="18"
+          viewBox="0 0 14 18"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10 6.25L4.5 11.75L2 9.25"
+            stroke="#006F51"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
         scheduled
       </span>
     ) : (
-      <span class="inline-flex items-center gap-x-1.5 rounded-full bg-red-500 px-3 py-1.5 text-xs font-medium text-white">
-        <LuX />
+      <span class=" inline-flex items-center gap-x-1.5 rounded-full bg-red-500 px-3 py-1.5 text-xs font-medium text-white">
+        <svg
+          width="14"
+          height="18"
+          viewBox="0 0 14 18"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M7.00501 7.50462V9.50306M7.00501 11.5015H7.01M11.8661 12.0011L7.86932 5.00655C7.78217 4.85278 7.65579 4.72487 7.50307 4.63588C7.35035 4.54689 7.17676 4.5 7.00001 4.5C6.82326 4.5 6.64967 4.54689 6.49695 4.63588C6.34423 4.72487 6.21785 4.85278 6.1307 5.00655L2.13389 12.0011C2.0458 12.1537 1.99961 12.3268 2 12.503C2.00039 12.6792 2.04735 12.8521 2.13611 13.0043C2.22488 13.1564 2.35229 13.2824 2.50544 13.3695C2.65859 13.4565 2.83204 13.5015 3.00819 13.5H11.0018C11.1771 13.4998 11.3493 13.4535 11.5011 13.3657C11.6528 13.2779 11.7788 13.1518 11.8664 12.9999C11.954 12.848 12 12.6758 12 12.5005C12 12.3252 11.9538 12.153 11.8661 12.0011Z"
+            stroke="#FEF2F2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
         cancelled
       </span>
     ),
@@ -240,11 +290,39 @@ export default component$(() => {
 
           {/* Model pop-up of logout */}
           <Modal isOpen={isOpen}>
-            <div class="flex flex-col items-center justify-center">
+            <div class="my-5 flex flex-col items-center justify-center space-y-12">
               <div>
-                <LuLogOut class="size-" />
+                <LuLogOut class="size-20 text-red-500" />
               </div>
-              <div>Do you confirm to logout?</div>
+              <div>
+                <p class="text-2xl">Do you confirm to logout?</p>
+              </div>
+              <div class="space-x-14">
+                <button
+                  type="button"
+                  onClick$={() => {
+                    isOpen.value = false;
+                  }}
+                  class="inline-flex h-11 w-28 items-center justify-center gap-x-2 rounded-full border border-transparent bg-red-500 px-4 py-3 text-sm font-medium text-white hover:bg-red-700 focus:bg-red-700 focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+                >
+                  Cancel
+                </button>
+                <button
+                  type="button"
+                  onClick$={async () => {
+                    const res = await logoutAction.submit();
+                    console.log(res);
+
+                    if (res.value.success) {
+                      await nav("/log_in/");
+                    }
+                  }}
+                  class="inline-flex h-11 w-28 items-center justify-center gap-x-2 rounded-full border border-transparent bg-slate-300 px-4 py-3 text-sm font-medium text-red-500 hover:bg-slate-500 focus:bg-slate-500  focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+                  disabled={logoutAction.isRunning}
+                >
+                  Log out
+                </button>
+              </div>
             </div>
           </Modal>
         </div>
