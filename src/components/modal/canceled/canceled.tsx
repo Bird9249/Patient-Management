63af075ba6251b0092edcc8885cade70ed9c7456
nadaongam <lattanadaongam@gmial.com>
2024-10-11 14:40:58 +0700

    change name fromStore to formCancelStore

--- src/components/modal/canceled/canceled.tsx
+++ src/components/modal/canceled/canceled.tsx
@@ -9,10 +9,10 @@ export const Cancel_Modal = component$<{ isOpen: Signal<boolean> }>(
   ({ isOpen }) => {
     const action = useCancelAppointmentAction();
     const nav = useNavigate();
-    const formStore = useStore({
+    const formCancelStore = useStore({
       reasonOfAdmin: "",
     });
-    const errorStore = useStore({
+    const errorCancelStore = useStore({
       reasonOfAdmin: "",
     });
 
@@ -20,7 +20,7 @@ export const Cancel_Modal = component$<{ isOpen: Signal<boolean> }>(
       <>
         <Modal isOpen={isOpen} header={{ title: "Cancel Appointment" }}>
           <div>
-            <h1 class="mx-4 text-start text-lg text-gray-400">
+            <h1 class="mx-4 text-start text-base text-gray-400">
               Are you sure you want to cancel your patient appointment
             </h1>
           </div>
@@ -29,14 +29,14 @@ export const Cancel_Modal = component$<{ isOpen: Signal<boolean> }>(
             <div class="flex-1">
               <Textarea
                 onInput$={(e, _) => {
-                  formStore.reasonOfAdmin = _.value;
+                  formCancelStore.reasonOfAdmin = _.value;
                 }}
                 name="reasonOfAdmin"
-                value={formStore.reasonOfAdmin}
+                value={formCancelStore.reasonOfAdmin}
                 label="Reason for cancellation"
                 placeholder="ex:we're sorry"
                 size="large"
-                error={errorStore.reasonOfAdmin}
+                error={errorCancelStore.reasonOfAdmin}
               />
             </div>
           </div>
@@ -46,16 +46,15 @@ export const Cancel_Modal = component$<{ isOpen: Signal<boolean> }>(
               class="mb-10 block w-full rounded-lg bg-red-600 px-5 py-3 text-white"
               onClick$={async () => {
                 const res = await action.submit({
-                  reasonOfAdmin: formStore.reasonOfAdmin,
+                  reasonOfAdmin: formCancelStore.reasonOfAdmin,
                 });
 
                 if (res.value.data.success) {
                   await nav(`/admin_page`);
                 } else {
                   if (res.value.data.type === "validate") {
-                    errorStore.reasonOfAdmin = res.value.data.errors!.nested![
-                      "reasonOfAdmin"
-                    ]
+                    errorCancelStore.reasonOfAdmin = res.value.data.errors!
+                      .nested!["reasonOfAdmin"]
                       ? res.value.data.errors!.nested!["reasonOfAdmin"][0]
                       : "";
                   }
